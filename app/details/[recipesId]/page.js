ab08623619b022bbe4f72d83883b836b5d965848
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-05-02 15:12:15 +0600

    open graph image link url check

--- app/details/[recipesId]/page.js
+++ app/details/[recipesId]/page.js
@@ -18,7 +18,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
         title: `khana-khajna | ${recipeDetails?.name}`,
         description: recipeDetails?.description?.slice(0, 100),
         openGraph: {
-            images: [recipeDetails?.thumbnail],
+            images: "https://source.unsplash.com/random/800x600?recipe",
         },
     }
 }
